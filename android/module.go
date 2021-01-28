c0b79a0e749ba4cbf4d8d5de0a92198abfa496fb
Manish4586 <manish.n.manish45@gmail.com>
2021-01-28 14:40:00 +0530

    Revert "Add hidden_api for java_import"
    This reverts commit cf12e289b38fedd72ba0697129ba45e38ae61f14.
    
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- android/module.go
+++ android/module.go
@@ -242,8 +242,6 @@ type Module interface {
 	InstallBypassMake() bool
 	SkipInstall()
 	IsSkipInstall() bool
-	ReplacedByPrebuilt()
-	IsReplacedByPrebuilt() bool
 	ExportedToMake() bool
 	InitRc() Paths
 	VintfFragments() Paths
@@ -525,9 +523,6 @@ type commonProperties struct {
 	CommonOSVariant bool `blueprint:"mutated"`
 
 	SkipInstall bool `blueprint:"mutated"`
-
-	// Whether the module has been replaced by a prebuilt
-	ReplacedByPrebuilt bool `blueprint:"mutated"`
 
 	// Disabled by mutators. If set to true, it overrides Enabled property.
 	ForcedDisabled bool `blueprint:"mutated"`
@@ -996,15 +991,6 @@ func (m *ModuleBase) SkipInstall() {
 
 func (m *ModuleBase) IsSkipInstall() bool {
 	return m.commonProperties.SkipInstall == true
-}
-
-func (m *ModuleBase) ReplacedByPrebuilt() {
-	m.commonProperties.ReplacedByPrebuilt = true
-	m.SkipInstall()
-}
-
-func (m *ModuleBase) IsReplacedByPrebuilt() bool {
-	return m.commonProperties.ReplacedByPrebuilt
 }
 
 func (m *ModuleBase) ExportedToMake() bool {
